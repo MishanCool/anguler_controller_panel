cbc6a53ea2db67584f72688b75daba53acc3c8db
Mishan Madhupa <mishancool2016@gmail.com>
2019-09-10 23:01:21 +0530

    Import routes and set conection corectly

--- angulerControlPanel/src/app/app.module.ts
+++ angulerControlPanel/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
+// components
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ControlPanelComponent } from './components/control-panel/control-panel.component';
+
+// routes
+import { appRoutes } from './routes/routes';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ControlPanelComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
